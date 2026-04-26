17984953318b9b1b494947c557d3848bfcd8eaf0
Eyebeck <07zhantumayev@gmail.com>
2026-04-26 18:42:51 +0500

    корзина и контекст процессор

--- shop/main/urls.py
+++ shop/main/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path
 from . import views
+from .views import dashboard_view, login_view, logout_view, register_view
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'main'
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
-    path('<slug:category_slug>/', views.product_list,
-         name='product_list_by_category'),
-    path('<int:id>/<slug:slug>', views.product_detail,
-         name='product_detail'),
+    path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
+    path('<int:id>/<slug:slug>', views.product_detail, name='product_detail'),
+    path("register/", register_view, name="register"),
+    path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
+    path("dashboard/", dashboard_view, name="dashboard"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- shop/main/views.py
+++ shop/main/views.py
@@ -1,5 +1,47 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
+from cart.forms import CartAddProductForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.shortcuts import redirect, render
+
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("dashboard")
+    else:
+        form = UserCreationForm()
+
+    return render(request, "blog/register.html", {"form": form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect("dashboard")
+    else:
+        form = AuthenticationForm()
+
+    return render(request, "blog/login.html", {"form": form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
+
+
+@login_required(login_url="login")
+def dashboard_view(request):
+    return render(request, "blog/dashboard.html")
 
 def product_list(request, category_slug=None):
     categories = Category.objects.all()
@@ -20,5 +62,8 @@ def product_detail(request, id, slug):
     related_products = Product.objects.filter(category=product.category,
                                               available=True).exclude(id=product.id)[:4]
 
+    cart_product_form = CartAddProductForm()
+
     return render(request, 'main/product/detail.html', {'product': product,
-                                                        'related_products' : related_products})
+                                                        'related_products' : related_products,
+                                                        'cart_product_form': cart_product_form})
